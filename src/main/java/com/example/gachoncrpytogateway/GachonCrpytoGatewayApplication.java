5dcc728ad75d4540db960b1c03f720b72b2ede52
RyuKwanKon <rhks33rhs@gmail.com>
2023-10-31 16:00:59 +0900

    [init] project 초기 세팅

---
+++ src/main/java/com/example/gachoncrpytogateway/GachonCrpytoGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.example.gachoncrpytogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GachonCrpytoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GachonCrpytoGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gachoncrpytogateway/GachonCrpytoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gachoncrpytogateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachonCrpytoGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
